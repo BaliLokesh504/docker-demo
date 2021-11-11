00e5c7e18ad66d399467c522763bc124affb0082
BaliLokesh504 <balilokesh20@gmail.com>
2021-11-11 10:44:35 +0530

    all

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Bye there');
 });
 
-//
+//ssh-keygen -t rsa -b 4096 -C "balilokesh20@gmail.com"
 app.listen(8080, () => {
   console.log('Listening on port 8080');
 });
